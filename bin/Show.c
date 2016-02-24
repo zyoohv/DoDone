553e37861d29c11c3611232917c39baee431f6b8
zyoohv <zyoohv@localhost.localdomain>
2016-02-24 21:54:51 +0800

    24 Feb update

---
+++ bin/Show.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+#define buflen 50
+
+char files[35][15];
+int all[35],finish[35];
+char buf[155];
+
+int main(int argc,char *argv[]){
+
+	freopen(argv[1],"r",stdin);
+	int cnt = 0;
+	while(scanf("%s",files[cnt]) != EOF) cnt++;
+	//printf("test1----\n");
+	//for(int i=0;i<cnt;i++) printf("%s\n",files[i]);
+
+	int len,slen,tot;
+	for(int i=0;i<cnt;i++){
+		len = strlen(argv[2]);
+		for(int j=0;j<len;j++) buf[j] = argv[2][j];
+
+		slen = strlen(files[i]);
+		tot = len;
+		for(int j=0;j<slen;j++) buf[tot++] = files[i][j];
+		buf[tot] = '\0';
+
+		freopen(buf,"r",stdin);
+		scanf("%d",&all[i]);
+
+		//----------------------------
+
+		len = strlen(argv[3]);
+		for(int j=0;j<len;j++) buf[j] = argv[3][j];
+		
+		slen = strlen(files[i]);
+		tot = len;
+		for(int j=0;j<slen;j++) buf[tot++] = files[i][j];
+		buf[tot] = '\0';
+
+		freopen(buf,"r",stdin);
+		scanf("%d",&finish[i]);
+	}
+	//printf("---test\n");
+	//for(int i=0;i<cnt;i++) printf("all : %d finish : %d\n",all[i],finish[i]);
+
+	freopen(argv[4],"w",stdout);
+	for(int i=0;i<cnt;i++){
+		printf("%s:[",files[i]);
+		if(all[i] == 0){
+			for(int j=0;j<buflen;j++) printf("#");
+			printf("]100%\n");
+			continue;
+		}
+		int num = buflen * finish[i] / all[i];
+		for(int j=0;j<num;j++) printf("#");
+		for(int j=0;j<buflen-num;j++) printf(".");
+		printf("]%d%\n",100 * finish[i] / all[i]);
+	}
+	return 0;
+}
+
---
+++ bin/Sortfile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+#define delnum 38
+
+char tmp[20][101];
+
+
+int main(int argc, char *argv[]){
+	freopen(argv[1],"r",stdin);
+	int cnt = 0;
+	while(gets(tmp[cnt])) cnt++;
+	//printf("argc = %d\n",argc);
+	//printf("argc = %s\n",argv[0]);
+	if(argc == 3){
+		int len = strlen(argv[2]);
+		int del = 0;
+		for(int i=0;i<len;i++){
+			del *= 10;
+			del += argv[2][i] - '0';
+		}
+		tmp[del-1][0] = delnum;
+	}
+	for(int i=0;i<cnt;i++)if(tmp[i][0] == delnum){
+		int nex = i + 1;
+		for(;nex < cnt;nex++)if(tmp[nex][0] != delnum) break;
+		if(nex >= cnt) break;
+		for(int j=0;j<101;j++){
+			char sw = tmp[i][j];
+			tmp[i][j] = tmp[nex][j];
+			tmp[nex][j] = sw;
+		}
+	}
+	for(int i=0;i<cnt;i++){
+		if(tmp[i][0] != delnum) tmp[i][0] = '1' + i;
+		else break;	
+	}
+	freopen(argv[1],"w",stdout);
+	for(int i=0;i<cnt;i++) printf("%s\n",tmp[i]);
+	return 0;
+}
